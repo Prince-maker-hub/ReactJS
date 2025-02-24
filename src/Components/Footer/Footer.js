a46b8735a22549c252a022ed5aabd86beea2111b
Prince Kumar Singh <119484969+Prince-maker-hub@users.noreply.github.com>
2025-02-25 00:32:56 +0530

    project

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer_container">
+      @Tripkaro 2025 | Prince Kumar Singh
+    </div>
+  )
+}
+
+export default Footer
